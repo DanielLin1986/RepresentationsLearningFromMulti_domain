0f5e356a408c1f653f9f7a07799fa2c1c4316b97
DanielLin1986 <yuyu-0626@hotmail.com>
2018-12-01 20:24:36 +1100

    Update Train_BiLSTM.py

--- Code/Train_BiLSTM.py
+++ Code/Train_BiLSTM.py
@@ -113,25 +113,25 @@ def ListToCSV(list_to_csv, path):
 
 # When test on project FFmpeg, we use the other 5 projects as training set.    
 #------------------------------------------------------------#    
-libtiff_list = LoadSavedData(working_dir + '\\TII\\FFmpeg\\5-project-except-ffmpeg\\5-project-funcs\\libtiff_list.pkl')
-libtiff_list_id = LoadSavedData(working_dir + '\\TII\\FFmpeg\\5-project-except-ffmpeg\\5-project-funcs\\libtiff_list_id.pkl')
-libtiff_label = LoadSavedData(working_dir + '\\TII\\FFmpeg\\5-project-except-ffmpeg\\5-project-funcs\\libtiff_label.pkl')
+libtiff_list = LoadSavedData(working_dir + '\\libtiff_list.pkl')
+libtiff_list_id = LoadSavedData(working_dir + '\\libtiff_list_id.pkl')
+libtiff_label = LoadSavedData(working_dir + '\\libtiff_label.pkl')
 
-libpng_list = LoadSavedData(working_dir + '\\TII\\FFmpeg\\5-project-except-ffmpeg\\5-project-funcs\\libpng_list.pkl')
-libpng_list_id = LoadSavedData(working_dir + '\\TII\\FFmpeg\\5-project-except-ffmpeg\\5-project-funcs\\libpng_list_id.pkl')
-libpng_label = LoadSavedData(working_dir + '\\TII\\FFmpeg\\5-project-except-ffmpeg\\5-project-funcs\\libpng_label.pkl')
+libpng_list = LoadSavedData(working_dir + '\\libpng_list.pkl')
+libpng_list_id = LoadSavedData(working_dir + '\\libpng_list_id.pkl')
+libpng_label = LoadSavedData(working_dir + '\\libpng_label.pkl')
 
-pidgin_list = LoadSavedData(working_dir + '\\TII\\FFmpeg\\5-project-except-ffmpeg\\5-project-funcs\\pidgin_list.pkl')
-pidgin_list_id = LoadSavedData(working_dir + '\\TII\\FFmpeg\\5-project-except-ffmpeg\\5-project-funcs\\pidgin_list_id.pkl')
-pidgin_label = LoadSavedData(working_dir + '\\TII\\FFmpeg\\5-project-except-ffmpeg\\5-project-funcs\\pidgin_label.pkl')
+pidgin_list = LoadSavedData(working_dir + '\\pidgin_list.pkl')
+pidgin_list_id = LoadSavedData(working_dir + '\\pidgin_list_id.pkl')
+pidgin_label = LoadSavedData(working_dir + '\\pidgin_label.pkl')
 
-vlc_list = LoadSavedData(working_dir + '\\TII\\FFmpeg\\5-project-except-ffmpeg\\5-project-funcs\\vlc_list.pkl')
-vlc_list_id = LoadSavedData(working_dir + '\\TII\\FFmpeg\\5-project-except-ffmpeg\\5-project-funcs\\vlc_list_id.pkl')
-vlc_label = LoadSavedData(working_dir + '\\TII\\FFmpeg\\5-project-except-ffmpeg\\5-project-funcs\\vlc_label.pkl')
+vlc_list = LoadSavedData(working_dir + '\\vlc_list.pkl')
+vlc_list_id = LoadSavedData(working_dir + '\\vlc_list_id.pkl')
+vlc_label = LoadSavedData(working_dir + '\\vlc_label.pkl')
 
-asterisk_list = LoadSavedData(working_dir + '\\TII\\FFmpeg\\5-project-except-ffmpeg\\5-project-funcs\\asterisk_list.pkl')
-asterisk_list_id = LoadSavedData(working_dir + '\\TII\\FFmpeg\\5-project-except-ffmpeg\\5-project-funcs\\asterisk_list_id.pkl')
-asterisk_label = LoadSavedData(working_dir + '\\TII\\FFmpeg\\5-project-except-ffmpeg\\5-project-funcs\\asterisk_label.pkl')
+asterisk_list = LoadSavedData(working_dir + '\\asterisk_list.pkl')
+asterisk_list_id = LoadSavedData(working_dir + '\\asterisk_list_id.pkl')
+asterisk_label = LoadSavedData(working_dir + '\\asterisk_label.pkl')
 
 total_list = libtiff_list + libpng_list + pidgin_list + vlc_list + asterisk_list
 total_list_id = libtiff_list_id + libpng_list_id + pidgin_list_id + vlc_list_id + asterisk_list_id
@@ -147,18 +147,18 @@ for item in empty_index_list:
     del total_list_id[item]
     del total_list_label[item]
 
-# We use FFmpeg as the test set.
-#------------------------------------------------------------#
+# # We use FFmpeg as the test set.
+# #------------------------------------------------------------#
 
-ffmpeg_train_list, ffmpeg_train_list_id = getCFilesFromText(working_dir + '\\TII\\FFmpeg\\5-project-except-ffmpeg\\train_func\\')
-ffmpeg_train_label = GenerateLabels(ffmpeg_train_list_id).flatten()
+# ffmpeg_train_list, ffmpeg_train_list_id = getCFilesFromText(working_dir + '\\FFmpeg\\train_func\\')
+# ffmpeg_train_label = GenerateLabels(ffmpeg_train_list_id).flatten()
 
-ffmpeg_train_list, ffmpeg_train_empty_list = ProcessList(removeSemicolon(ffmpeg_train_list)) 
+# ffmpeg_train_list, ffmpeg_train_empty_list = ProcessList(removeSemicolon(ffmpeg_train_list)) 
 
-ffmpeg_test_list, ffmpeg_test_list_id = getCFilesFromText(working_dir + '\\TII\\FFmpeg\\5-project-except-ffmpeg\\test_func\\')
-ffmpeg_test_label = GenerateLabels(ffmpeg_test_list_id).flatten()
+# ffmpeg_test_list, ffmpeg_test_list_id = getCFilesFromText(working_dir + '\\FFmpeg\\test_func\\')
+# ffmpeg_test_label = GenerateLabels(ffmpeg_test_list_id).flatten()
 
-ffmpeg_test_list, ffmpeg_test_empty_list = ProcessList(removeSemicolon(ffmpeg_test_list)) 
+# ffmpeg_test_list, ffmpeg_test_empty_list = ProcessList(removeSemicolon(ffmpeg_test_list)) 
 # 2. Load the Tokenizer and the trained word2vec model
 #--------------------------------------------------------
 
@@ -172,14 +172,14 @@ def JoinSubLists(list_to_join):
     return new_list
 
 new_total_list =  JoinSubLists(total_list)
-new_ffmpeg_train_list = JoinSubLists(ffmpeg_train_list)
-new_ffmpeg_test_list = JoinSubLists(ffmpeg_test_list)
-#tokenizer = LoadSavedData(working_dir + 'common_tokenizer.pickle') # Baseline 2
-tokenizer = LoadSavedData('D:\\Phd\\Backup\\2018-10-16-TIFS\\all_tokenizer_no_comments.pickle') # Baseline 1
+# new_ffmpeg_train_list = JoinSubLists(ffmpeg_train_list)
+# new_ffmpeg_test_list = JoinSubLists(ffmpeg_test_list)
+
+tokenizer = LoadSavedData('D:\\Path\\to\\tokenizer.pickle') # Baseline 1
 
 total_sequences = tokenizer.texts_to_sequences(new_total_list)
-ffmpeg_train_sequences = tokenizer.texts_to_sequences(new_ffmpeg_train_list)
-ffmpeg_test_sequences = tokenizer.texts_to_sequences(new_ffmpeg_test_list)
+# ffmpeg_train_sequences = tokenizer.texts_to_sequences(new_ffmpeg_train_list)
+# ffmpeg_test_sequences = tokenizer.texts_to_sequences(new_ffmpeg_test_list)
 word_index = tokenizer.word_index
 print ('Found %s unique tokens.' % len(word_index))
 
@@ -187,8 +187,7 @@ print ("The length of tokenized sequence: " + str(len(total_sequences)))
 
 # 2.2 Load the trained word2vec model.
 
-#w2v_model_path = working_dir + 'CWE_w2v_model_CBOW.txt'# Baseline 2
-w2v_model_path = 'D:\\Phd\\Backup\\2018-10-16-TIFS\\all_w2v_model_CBOW_no_comments.txt'# Baseline 1
+w2v_model_path = 'D:\\Path\\to\\w2v_model.txt'# Baseline 1
 
 w2v_model = open(w2v_model_path)
 
@@ -200,7 +199,7 @@ print (w2v_model)
 # Plot tokenized sequences.
 length_list = []
 
-for item in total_sequences + ffmpeg_train_sequences + ffmpeg_test_sequences:
+for item in total_sequences:
     length_list.append(len(item))
 
 plt.hist(length_list, bins=30)
@@ -215,17 +214,17 @@ print ("max_len ", MAX_LEN)
 print('Pad sequences (samples x time)')
 
 total_sequences_pad = pad_sequences(total_sequences, maxlen = MAX_LEN, padding ='post')
-ffmpeg_train_sequence_pad = pad_sequences(ffmpeg_train_sequences, maxlen = MAX_LEN, padding ='post')
-ffmpeg_test_sequence_pad = pad_sequences(ffmpeg_test_sequences, maxlen = MAX_LEN, padding ='post')
+# ffmpeg_train_sequence_pad = pad_sequences(ffmpeg_train_sequences, maxlen = MAX_LEN, padding ='post')
+# ffmpeg_test_sequence_pad = pad_sequences(ffmpeg_test_sequences, maxlen = MAX_LEN, padding ='post')
 
 print ("The shape after paddings: ")
 print (total_sequences_pad.shape)
 
-print ("The shape after paddings: ")
-print (ffmpeg_train_sequence_pad.shape)
+# print ("The shape after paddings: ")
+# print (ffmpeg_train_sequence_pad.shape)
 
-print ("The shape after paddings: ")
-print (ffmpeg_test_sequence_pad.shape)
+# print ("The shape after paddings: ")
+# print (ffmpeg_test_sequence_pad.shape)
 
 #-------------------------------------------------------------#
 # Use part of the ffmpeg training set as the actual training set, and the remaining set as the validation set.  
@@ -234,10 +233,9 @@ ffmpeg_train_set_x, ffmpeg_validation_set_x, ffmpeg_train_set_y, ffmpeg_validati
 #
 #train_set_x, validation_set_x, train_set_y, validation_set_y, train_set_id, validation_set_id = train_test_split(train_set_x, train_set_y, train_set_id, test_size=0.3, random_state=42)
 
-train_set_x = total_sequences_pad.tolist() + ffmpeg_train_set_x.tolist()
+train_set_x = total_sequences_pad.tolist()
 train_set_x = np.asarray(train_set_x)
 validation_set_x = ffmpeg_validation_set_x 
-test_set_x = ffmpeg_test_sequence_pad
 
 print ("Training set: ")
 
@@ -247,19 +245,14 @@ print ("Validation set: ")
 
 print (validation_set_x)
 
-print ("Test set: ")
-
-print (test_set_x)
-
-train_set_y = np.asarray(total_list_label + ffmpeg_train_set_y.tolist())
+train_set_y = np.asarray(total_list_label)
 validation_set_y = np.asarray(ffmpeg_validation_set_y)
-test_set_y = np.asarray(ffmpeg_test_label)
 
-print (len(train_set_x), len(train_set_y), len(total_list_id + ffmpeg_train_set_id), len(validation_set_y), len(ffmpeg_validation_set_id), len(validation_set_x), len(test_set_x), len(test_set_y), len(ffmpeg_test_list_id))
+print (len(train_set_x), len(train_set_y), len(total_list_id + ffmpeg_train_set_id), len(validation_set_y), len(ffmpeg_validation_set_id), len(validation_set_x))
 
-print (train_set_x.shape, train_set_y.shape, validation_set_x.shape, validation_set_y.shape, test_set_x.shape, test_set_y.shape)
+print (train_set_x.shape, train_set_y.shape, validation_set_x.shape, validation_set_y.shape)
 
-print (np.count_nonzero(train_set_y), np.count_nonzero(validation_set_y), np.count_nonzero(test_set_y))
+print (np.count_nonzero(train_set_y), np.count_nonzero(validation_set_y))
 
 # -----------------------------------
 # 4. Preparing the Embedding layer
@@ -277,39 +270,16 @@ w2v_model.close()
 
 print('Found %s word vectors.' % len(embeddings_index))
 
-#embedding_matrix = np.zeros((len(word_index) + 1, EMBEDDING_DIM)) # The shape of embedding matrix (137, 100) because there are 137 common tokens.
-#for word, i in word_index.items():
-#   embedding_vector = embeddings_index.get(word) # the shape of the embedding_vector (100, )
-#   if embedding_vector is not None:
-#       # words not found in embedding index will be all-zeros.
-#       embedding_matrix[i] = embedding_vector
-
-# ====================================
-# Get common tokens label       
-# ====================================
-#common_tokens = LoadSavedData(working_dir + 'common_token_list.pkl')
-
-common_tokens = construc_list() 
-#
-embedding_matrix_1 = np.zeros((len(word_index) + 1, EMBEDDING_DIM + 1)) # The shape of embedding matrix (137, 100) because there are 137 common tokens.
+embedding_matrix = np.zeros((len(word_index) + 1, EMBEDDING_DIM)) # The shape of embedding matrix (137, 100) because there are 137 common tokens.
 for word, i in word_index.items():
-   embedding_vector = embeddings_index.get(word) # the shape of the embedding_vector (100, )
-   if word in common_tokens:
-       common_token_label = 1
-   else:
-       common_token_label = 0
-   if embedding_vector is not None:
-       embedding_vector_list = embedding_vector.tolist()
-       embedding_vector_list.append(common_token_label)
-       embedding_vector_arr = np.asarray(embedding_vector_list)
-       # words not found in embedding index will be all-zeros.
-       embedding_matrix_1[i] = embedding_vector_arr
-
-class_weight = {0:1, 1:8}
+  embedding_vector = embeddings_index.get(word) # the shape of the embedding_vector (100, )
+  if embedding_vector is not None:
+      # words not found in embedding index will be all-zeros.
+      embedding_matrix[i] = embedding_vector
 
 def train(train_set_x, train_set_y, validation_set_x, validation_set_y, saved_model_name):
     
-    model = BiLSTM_network(MAX_LEN, EMBEDDING_DIM + 1, word_index, embedding_matrix_1, True)
+    model = BiLSTM_network(MAX_LEN, EMBEDDING_DIM, word_index, embedding_matrix, True)
     
     callbacks_list = [
        ModelCheckpoint(filepath = model_saved_path + os.sep + saved_model_name +'_{epoch:02d}_{val_acc:.3f}_{val_loss:3f}.h5', monitor='val_loss', verbose=1, save_best_only=True, period=1),
@@ -366,10 +336,6 @@ def test(test_set_x, test_set_y, model):
     
     return probs, test_accuracy
     
-#def train_multip_inputs():
-    
-#def test_multip_inputs():
-    
 
 def plot_history(network_history):
     plt.figure()
@@ -378,23 +344,11 @@ def plot_history(network_history):
     plt.plot(network_history.history['loss'])
     plt.plot(network_history.history['val_loss'])
     plt.legend(['Training', 'Validation'])
-    plt.savefig('Epoch_loss_class_weight.jpg') 
-    
-#def printAttentionWords(test_set_x, test_set_y, model, word_index, num_word_pay_atten):
-#    
-#    vul_index = []
-#    
-#    for index, item in enumerate(test_set_y):
-#        if item == 1:
-#            vul_index.append(index)
-#    
-#    # Only visualize the vulnerable sequences. 
-#    for item in vul_index:
-#        visualize_attention(test_set_x, item, model, word_index, num_word_pay_atten)
+    plt.savefig('Epoch_loss.jpg') 
 
 if __name__ == '__main__':
     
-    os.environ["CUDA_VISIBLE_DEVICES"]="0" # 1: Use the GTX 1070 only; 0: Use Titan XP only.
+    os.environ["CUDA_VISIBLE_DEVICES"]="0" #0: Use Titan XP only.
 
     model, train_history = train(train_set_x, train_set_y, validation_set_x, validation_set_y, 'BiLSTM_MA_part_of_ffmpeg_validation_class_weight_1')
     plot_history(train_history)
@@ -409,58 +363,8 @@ if __name__ == '__main__':
     
     #printAttentionWords(test_set_x, test_set_y, best_model, word_index, 20)
     
-    ListToCSV(probs.tolist(), working_dir + '//prob_nn_part_of_ffmpeg_validation.csv')
+    ListToCSV(probs.tolist(), working_dir + '//prob_nn_ffmpeg_validation.csv')
     ListToCSV(ffmpeg_test_label, working_dir + '//prob_nn_label.csv')
     ListToCSV(ffmpeg_test_list_id, working_dir + '//prob_nn_id_new_embedding.csv')
-    #K.clear_session()
+    K.clear_session()
 
-#-------------------------------------------------------------------------#
-# Get train and test set.
-    
-#working_dir_1 = 'D:\\Phd\\Backup\\2018-08-30-AsiaCCS\\manual_attention\\ffmpeg\\'    
-#
-#from os import listdir
-#from os.path import isfile, join
-#import shutil
-#
-#mypath = working_dir_1 + 'total_files\\'
-#file_list = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-#
-#for item in file_list:
-#    if item in test_set_id:
-#        shutil.copyfile(mypath + item + os.sep, working_dir_1 + 'Test\\' + item)
-#    else:
-#        shutil.copyfile(mypath + item + os.sep, working_dir_1 + 'Train\\' + item)
-#        
-"""
-The FFmpeg data training set was completely used as validation set. 
-
-[[3892    0]
- [ 145    0]]
-
-                precision    recall  f1-score   support
-
-Non-vulnerable       0.96      1.00      0.98      3892
-    Vulnerable       0.00      0.00      0.00       145
-
-     micro avg       0.96      0.96      0.96      4037
-     macro avg       0.48      0.50      0.49      4037
-  weighted avg       0.93      0.96      0.95      4037
-
-
-#------------------------------------------------------------#
-Part of the FFmpeg training set was used as actual training set, and the remaining was used as validation set.
-
-[[3892    0]
- [ 145    0]]
-
-                precision    recall  f1-score   support
-
-Non-vulnerable       0.96      1.00      0.98      3892
-    Vulnerable       0.00      0.00      0.00       145
-
-     micro avg       0.96      0.96      0.96      4037
-     macro avg       0.48      0.50      0.49      4037
-  weighted avg       0.93      0.96      0.95      4037
-  
-"""
